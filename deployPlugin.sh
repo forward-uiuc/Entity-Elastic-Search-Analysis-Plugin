a9631dca2ca9e687ddd334701732760ed3b2f56c
Long Pham <ltpham3@illinois.edu>
2018-08-21 16:14:08 -0500

    revise when writing installation guideline

--- deployPlugin.sh
+++ deployPlugin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn package -DskipTests
-/Users/longpham/Workspace/elasticsearch-5.6.1/bin/elasticsearch-plugin remove entity-elastic-search-analysis-plugin
-/Users/longpham/Workspace/elasticsearch-5.6.1/bin/elasticsearch-plugin install file:///Users/longpham/Workspace/Entity-Elastic-Search-Analysis-Plugin/target/releases/entity-elastic-search-analysis-plugin-5.6.1.zip
+/Users/longpham/Workspace/ForwardSearchInstallationGuide/elasticsearch-5.6.1/bin/elasticsearch-plugin remove entity-elastic-search-analysis-plugin
+/Users/longpham/Workspace/ForwardSearchInstallationGuide/elasticsearch-5.6.1/bin/elasticsearch-plugin install file:///Users/longpham/Workspace/ForwardSearchInstallationGuide/Entity-Elastic-Search-Analysis-Plugin/target/releases/entity-elastic-search-analysis-plugin-5.6.1.zip
